3c3bfe8d7b7738c77a603693d87bd38429b01dd1
fabuloussTeam <tjomemmanuel@gmail.com>
2020-06-29 10:19:25 +0100

    Mise en place des barChart en fonction des depense de la semaine: Bar Chart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/data/data.dart';
+import 'package:flutter_budget_ui/widgets/bar_chart.dart';
 // ignore: avoid_web_libraries_in_flutter
 
 class HomeScreen extends StatefulWidget {
@@ -18,16 +20,16 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverAppBar(
             forceElevated: true,
             floating: true,
-           // pinned: true,
+            //pinned: true,
             expandedHeight: 100.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('Simple Budget'),
-              centerTitle: true,
-            ),
             leading: IconButton(
               icon: Icon(Icons.settings),
               iconSize: 30.0,
               onPressed: (){},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget'),
+              centerTitle: true,
             ),
             actions: <Widget>[
               IconButton(
@@ -36,17 +38,28 @@ class _HomeScreenState extends State<HomeScreen> {
                 onPressed: (){},
               )
             ],
-
           ),
           SliverList(
               delegate: SliverChildBuilderDelegate(
                   (BuildContext context, int index){
                     return Container(
                       margin: EdgeInsets.all(10.0),
-                        height: 100.0,
-                        color: Colors.red,
+                      //  height: 100.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0, 2),
+                              blurRadius: 6.0
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      child: BarChart(weeklySpending),
                     );
                   },
+                childCount: 1,
               ),
           ),
         ],
@@ -54,3 +67,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
